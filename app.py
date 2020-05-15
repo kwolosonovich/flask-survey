16c1024d967860dc15035845d87c16a03e36d550
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-05-14 19:01:25 -0700

    added base.html and home.html variables and content

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template,  redirect, flash,  jsonify, session
 from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey, personality_quiz, Survey, Question
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
@@ -9,5 +10,6 @@ responses = []
 @app.route("/")
 def homepage():
     '''homepage with survey name, questions and start btn'''
-    # text = satisfaction_survey.text
-    return render_template("homepage.html")
+    instructions = satisfaction_survey.instructions
+    title = satisfaction_survey.title
+    return render_template("homepage.html", instructions=instructions, title=title)
