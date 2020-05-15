9de64d8b9e4ccc86e1e7cea259fd636acdde1d6b
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-05-15 15:50:28 -0700

    added question_id variable and revised questions()

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask, request, render_template,  redirect, flash,  jsonify, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey, personality_quiz, Survey, Question
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
+bootstrap = Bootstrap(app)
 
 responses = []
+question_id = len(responses)
+
 
 @app.route("/")
 def homepage():
@@ -13,3 +17,10 @@ def homepage():
     instructions = satisfaction_survey.instructions
     title = satisfaction_survey.title
     return render_template("homepage.html", instructions=instructions, title=title)
+
+@app.route("/questions/<int: question_id>")
+def questions():
+    '''renders the first survey question'''
+    question = satisfaction_survey.questions[question_id]
+
+    return render_template("questions.html", question_id=question_id, question=question)
