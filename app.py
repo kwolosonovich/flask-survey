ea32c199e418153e7e3ab464aa121a8bb5ebb5a6
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-05-14 17:40:02 -0700

    added homepage()

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
 from flask import Flask, request, render_template,  redirect, flash,  jsonify, session
 from flask_debugtoolbar import DebugToolbarExtension
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secret"
+
+responses = []
+
+@app.route("/")
+def homepage():
+    '''homepage with survey name, questions and start btn'''
+    text = satisfaction_survey.text
+    return render_template("home.html", text=text)
