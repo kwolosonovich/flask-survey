08aca3d0d5e7e35791017d4a6ec534c1d09a23c9
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-05-16 15:11:32 -0700

    added css formating

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, render_template,  redirect, flash,  jsonify, session
+from flask import Flask, request, render_template,  redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey, personality_quiz, Survey, Question
 from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
@@ -9,6 +10,7 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 bootstrap = Bootstrap(app)
 debug = DebugToolbarExtension(app)
 
+# allows new instances of survey results
 SESSION_KEY = "responses"
 
 @app.route("/")
@@ -21,35 +23,54 @@ def homepage():
 
 @app.route("/questions/<int:question_id>", methods=["GET", "POST"])
 def questions(question_id):
-    '''renders the first survey question'''
-
+    '''renders the first survey question. prevents user from skipping or resubmitting questions'''
     responses = session[SESSION_KEY]
 
-    if question_id > len(responses):
-        flash(f"Invalid question id: {question_id}")
-        return redirect(f"/questions/{len(responses)}")
-    elif len(responses) == len(satisfaction_survey.questions):
-        flash(f"Invalid question id: {question_id}")
-        return redirect("/survey_end")
+    if request.method in ["GET", "POST"]:
+        # check for invalid response
+        if question_id > len(responses):
+            flash(f"Invalid question id: {question_id}")
+            return redirect(f"/questions/{len(responses)}")
+        elif len(responses) == len(satisfaction_survey.questions):
+            flash(f"Invalid question id: {question_id}")
+            return redirect("/survey_end")
+        else:
+            question = satisfaction_survey.questions[question_id].question
+            choices = satisfaction_survey.questions[question_id].choices
+            return render_template("questions.html", question=question, choices=choices)
     else:
-        question = satisfaction_survey.questions[question_id].question
-        choices = satisfaction_survey.questions[question_id].choices
-        return render_template("questions.html", question=question, choices=choices)
+        # redirect to questions/len(responses
+        return redirect(f"/questions/{len(responses)}")
 
 @app.route("/responses", methods=["POST"])
 def collect_responses():
+    '''get answer and add to responses, redirects to the next question or survey_end page'''
     responses = session[SESSION_KEY]
-    answer = request.form["option"]
-    responses.append(answer)
-    session[SESSION_KEY] = responses
 
-    survey_length = len(satisfaction_survey.questions)
-    if survey_length != len(responses):
-        return redirect(f"/questions/{len(responses)}")
+    if request.method == "POST":
+        answer = request.form.get("option", None)
+        # check for invalid response
+        if not answer:
+            return redirect(f"/questions/{len(responses)}")
+
+        responses.append(answer)
+        session[SESSION_KEY] = responses
+        survey_length = len(satisfaction_survey.questions)
+
+        if survey_length != len(responses):
+            return redirect(f"/questions/{len(responses)}")
+        else:
+            return redirect("/survey_end")
     else:
-        return redirect("/survey_end")
+        # redirect to questions/len(responses)
+        return redirect(f"/questions/{len(responses)}")
 
 @app.route("/survey_end")
 def survey_end():
     '''notify user that survey has been completed'''
     return render_template("survey_end.html")
+
+@app.route("/summary")
+def summary():
+    '''temp route - placeholder for user summaries'''
+    return render_template("summary.html")
