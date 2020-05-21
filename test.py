50969cd16bda079c256d4a994b0a43992aad09d9
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-05-20 22:32:43 -0700

    added additional unit tests

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import unittest
 from app import app, homepage, questions, collect_responses, survey_end, summary
 from surveys import satisfaction_survey
-from flask import session
+from flask import session, jsonify
 
 class TestFlaskSurvey(unittest.TestCase):
     def setUp(self):
@@ -9,6 +9,8 @@ class TestFlaskSurvey(unittest.TestCase):
         app.config['TESTING'] = True
         app.config['SECRET_KEY'] = "secret"
         app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+        self.session_key = "responses"
+
 # test homepage route
     def test_get_method_on_homepage(self):
         '''Tests valid method'''
@@ -29,52 +31,74 @@ class TestFlaskSurvey(unittest.TestCase):
              response = client.put('/questions/0')
              self.assertTrue(response.status_code != 200)
 
-    def test_impossible_question_id(self):
+    def test_question_id_greater_than_number_of_responses(self):
         '''Test impossible guestion_id param'''
-        assert False
+        with self.client.session_transaction() as session:
+            session[self.session_key] = []
 
-    def test_impossible_response_len(self):
-        '''Test impossible response length'''
-        assert False
+        answers = session[self.session_key]
+        with self.client as client:
+            number_of_answers = len(answers)
+            response = client.post(f"/questions/{number_of_answers + 1}")
+            # should return re-direct status code
+            self.assertTrue(response.status_code == 302)
 
-    def test_impossible_satisfaction_survey_len(self):
-        '''Test impossible satisfaction survey length'''
-        assert False
+    def test_if_all_questions_answered(self):
+        with self.client.session_transaction() as session:
+            session[self.session_key] = [True] * len(satisfaction_survey.questions)
 
-    def test_invalid_redirect_function(self):
-        '''Test invalid redirect'''
-        assert False
-
-# test responses route
-    def test_get_method_on_responses(self):
-        '''Test invalid method'''
-        assert False
-
-    def test_impossible_responses(self):
-        '''Test impossible responses from session[SESSION_KEY]'''
-        assert False
-
-    def test_impossible_answer(self):
-        '''Test impossible answer from option'''
-        assert False
-
-    def test_impossible_survey_length(self):
-        '''Test impossible survey_length from satisfaction_survey.questions'''
-        assert False
-
-# test survey_end
-    def test_invalid_method_on_survey_end(self):
-        '''Test invalid method'''
-        assert False
-
-    def test_invalid_render_template_response(self):
-        '''Test invalid response'''
-        assert False
-
+        answers = session[self.session_key]
+        with self.client as client:
+            number_of_answers = len(answers)
+            response = client.post(f"/questions/{number_of_answers}")
+            self.assertTrue(response.status_code == 302)
+#
+#     def test_impossible_satisfaction_survey_len(self):
+#         '''Test impossible satisfaction survey length'''
+#         assert False
+#
+#     def test_invalid_redirect_function(self):
+#         '''Test invalid redirect'''
+#         assert False
+#
+# # test responses route
+#     def test_get_method_on_responses(self):
+#         '''Test invalid method'''
+#         with self.client as client:
+#             response = client.get('/responses')
+#             self.assertFalse(response.status_code == 200)
+#
+#
+#     def test_impossible_responses(self):
+#         '''Test impossible responses from session[SESSION_KEY]'''
+#         assert False
+#
+#     def test_impossible_answer(self):
+#         '''Test impossible answer from option'''
+#         assert False
+#
+#     def test_impossible_survey_length(self):
+#         '''Test impossible survey_length from satisfaction_survey.questions'''
+#         assert False
+#
+# # test survey_end
+#     def test_invalid_method_on_survey_end(self):
+#         '''Test invalid method'''
+#         with self.client as client:
+#             response = client.get('/survey_end')
+#             self.assertFalse(response.status_code != 200)
+#
+#     def test_invalid_render_template_response(self):
+#         '''Test invalid response'''
+#         assert False
+#
     def test_invalid_method_on_summary(self):
         '''Test invalid method'''
-        assert False
+        with self.client as client:
+            response = client.post('/summary')
+            self.assertTrue(response.status_code != 200)
 
-    def test_invalid_render_template_response(self):
-        '''Test invalid response'''
-        assert False
+    def test_valid_method_on_summary(self):
+        with self.client as client:
+            response = client.get('/summary')
+            self.assertTrue(response.status_code == 200)
