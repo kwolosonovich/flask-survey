23b2cd393614bb300f8b957741854696d02dac9f
kelly wolosonovich <k.wolosonovich@gmail.com>
2020-05-20 20:48:41 -0700

    updated docstrings

--- test.py
+++ test.py
@@ -11,20 +11,21 @@ class TestFlaskSurvey(unittest.TestCase):
         app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
     def test_get_method_on_homepage(self):
+        '''tests valid method'''
         with self.client as client:
-            '''tests valid method'''
             response = client.get('/')
             self.assertTrue(response.status_code == 200)
 
     def test_post_method_on_homepage(self):
+        '''tests invalid request method'''
         with self.client as client:
-            '''tests invalid request method'''
             response = client.post('/')
             self.assertTrue(response.status_code != 200)
 
     def test_invalid_method_on_questions(self):
+        '''tests invalid requests method'''
         with self.client as client:
-            '''tests invalid requests method'''
              response = client.put('/questions/0')
              self.assertTrue(response.status_code != 200)
+
 
